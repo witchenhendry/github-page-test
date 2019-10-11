9300fb7abe690a21691b2afd8298282a3a423f37
jrx <jrx@gmail.com>
2019-10-11 16:57:09 +0800

    Updates

---
+++ precache-manifest.67ba51034386784847d410adf2be4c4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7299fc8eedae85f03f6a2646e1b68d10",
+    "url": "/github-page-test/index.html"
+  },
+  {
+    "revision": "2135d766df980d1c9a92",
+    "url": "/github-page-test/static/css/main.7b789137.chunk.css"
+  },
+  {
+    "revision": "b4ac3d224c8ccdc70889",
+    "url": "/github-page-test/static/js/2.aade5e3f.chunk.js"
+  },
+  {
+    "revision": "2135d766df980d1c9a92",
+    "url": "/github-page-test/static/js/main.5072241e.chunk.js"
+  },
+  {
+    "revision": "ca559e71bdecde8e70f7",
+    "url": "/github-page-test/static/js/runtime-main.3d9ad62a.js"
+  }
+]);
